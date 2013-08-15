502564ba84b94e59f8f8a0139f58815dab1561cc
emmatommy <emmatommy@live.com.mx>
2013-08-15 15:27:16 -0500

    intento

--- matFuncion.c
+++ matFuncion.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<time.h>
+#define R 6
+#define C 6
 
-void rellenar(int []);
-void imprimir(int []);
+void rellenar(int [][C]);//Se le debe indicar siempre el numero de columnas
+void imprimir(int [][C]);
 
 void main(){
- int  a[6];
+ int  a[R][C]={0};
 
 rellenar(a);
 imprimir(a);
@@ -14,19 +15,22 @@ imprimir(a);
 
 } 
 
-void rellenar(int a [6]){
-	int i;
-	for(i=0;i<6;i++)
-		a[i]=rand() % 10;
-	
+void rellenar(int a[R][C]){
+	int i,j;
+	srand(time(NULL));//genera numeros aleatorios tomandola hora del equipo
+	for(i=0;i<R;i++){
+		for(j=0;j<C;j++)
+		a[i][j]=rand() % 10; //genera numero psudoaleatorios
+	}
 
 
 }
-void imprimir(int a [6]){
-	int i;
-	for(i=0;i<6;i++)
-	printf("%d",a[i]);
-	
+void imprimir(int a [R][C]){
+	int i,j;
 
-
-}
+	for(i=0;i<R;i++){
+		for(j=0;j<C;j++)
+			printf("%d\t",a[i][j]);
+		puts("");
+			}
+	}
